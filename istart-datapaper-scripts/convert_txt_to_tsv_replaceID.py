128c8c17f2e2954b4391000e466aa0002eefcc12
cooper-sharp <tun31934@temple.edu>
2023-10-20 01:14:50 -0400

    Update convert_txt_to_tsv_replaceID.py
    changed variables and function names for clarity
    
    changed formatting; next commit should include comments above functions

--- istart-datapaper-scripts/convert_txt_to_tsv_replaceID.py
+++ istart-datapaper-scripts/convert_txt_to_tsv_replaceID.py
@@ -2,25 +2,31 @@ import os
 import pandas as pd
 import csv
 
-def read_target_numbers(filename):
-    with open(filename, 'r') as file:
-        target_numbers = [line.strip() for line in file]
-    return target_numbers
 
-def filter_rows(filename, target_numbers):
+
+def read_usable_subs(filename):
+    with open(filename, 'r') as file:
+        usable_subs = [line.strip() for line in file]
+    return usable_subs
+
+
+
+def eliminate_subs(filename, usable_subs):
     with open(filename, 'r') as file:
         lines = file.readlines()
 
-    header = lines[0]  # Save the header
-    data_lines = lines[1:]  # Exclude the header from processing
+    header = lines[0]  
+    data_lines = lines[1:]  
 
-    filtered_data_lines = [line for line in data_lines if any(line.strip().startswith(target) for target in target_numbers)]
+    filtered_data_lines = [line for line in data_lines if any(line.strip().startswith(id) for id in usable_subs)]
 
     with open(filename, 'w') as file:
-        file.write(header)  # Rewrite the header
+        file.write(header)  
         file.writelines(filtered_data_lines)
 
-def format_first_row(filename):
+
+
+def format_first_column(filename):
     with open(filename, 'r') as infile:
         reader = csv.reader(infile)
         rows = list(reader)
@@ -28,7 +34,6 @@ def format_first_row(filename):
     if rows:
         for row in rows[1:]:
             if row and len(row) > 0:
-                # Add "sub-" prefix to the first column
                 row[0] = f'sub-{row[0]}'
 
     with open(filename, 'w', newline='') as infile:
@@ -46,12 +51,13 @@ def format_first_row(filename):
         infile.write(header)
         infile.writelines(lines[1:])
     
-def process_files(folder_path, target_numbers):
+
+
+def process_files(folder_path, usable_subs):
     for filename in os.listdir(folder_path):
-        if filename.endswith('.txt'):
+        if filename.endswith('.tsv'):
             file_path = os.path.join(folder_path, filename)
 
-            # Replace "ID" with "participant_id" in the first row of each file
             with open(file_path, 'r') as infile:
                 reader = csv.reader(infile)
                 rows = list(reader)
@@ -60,12 +66,12 @@ def process_files(folder_path, target_numbers):
                 writer = csv.writer(outfile)
                 writer.writerows(rows)
 
-            filter_rows(file_path, target_numbers)
-
-            # Add "sub-" prefix to the first column
-            format_first_row(file_path)
+            eliminate_subs(file_path, usable_subs)
+            format_first_column(file_path)
 
             print(f"Processed: {file_path}")
+
+
 
 def convert_to_tsv(input_path, output_path):
     for filename in os.listdir(input_path):
@@ -73,16 +79,17 @@ def convert_to_tsv(input_path, output_path):
             txt_file_path = os.path.join(input_path, filename)
             tsv_file_path = os.path.join(output_path, filename.replace('.txt', '.tsv'))
 
-            df = pd.read_csv(txt_file_path, sep='\t', na_values=["n/a"])  # Read the processed .txt file with "N/A" values
-            df.to_csv(tsv_file_path, sep='\t', index=False, na_rep="n/a")  # Convert and save as .tsv with "N/A" values
+            df = pd.read_csv(txt_file_path, sep='\t', na_values=["n/a"])
+            df.to_csv(tsv_file_path, sep='\t', index=False, na_rep="n/a")
 
             print(f"Converted: {txt_file_path} to {tsv_file_path}")
 
+
+
 if __name__ == "__main__":
-    target_numbers = read_target_numbers('/Users/coopersharp/Desktop/usable_subs.txt')
+    usable_subs = read_usable_subs('/Users/coopersharp/Desktop/usable_subs.txt')
     input_path = '/Users/coopersharp/Desktop/rawdata'  # Specify the folder path containing the .txt files
     output_path = '/Users/coopersharp/Desktop/data_paper'
 
-    process_files(input_path, target_numbers)
     convert_to_tsv(input_path, output_path)
-
+    process_files(output_path, usable_subs)
