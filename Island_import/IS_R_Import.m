a9261b4a9fcb16ac744d834f5856100ed930b231
Avi <114612259+Avi-Dachs@users.noreply.github.com>
2023-11-16 15:08:50 -0500

    import code update

--- Island_import/IS_R_Import.m
+++ Island_import/IS_R_Import.m
@@ -2,47 +2,65 @@
 input_data = readtable("/Users/avidachs/Documents/GitHub/Scripts-CS/Island_import/input.csv");
 
 %READING THE TEMPLATE
-template_data =  readtable("/Users/avidachs/Documents/GitHub/Scripts-CS/Island_import/template.csv");
+template_data = readtable("/Users/avidachs/Documents/GitHub/Scripts-CS/Island_import/template.csv");
 
-%GENERATING THE OUTPUT
 % Extract the variable names from the first row of template_data
 templateVarNames = template_data.Properties.VariableNames;
 
-% Initialize an empty cell array for the data
-outputDataCell = cell(1, numel(templateVarNames));
+% Initialize an empty cell array to store output data
+outputDataCell = cell(height(input_data), numel(templateVarNames));
 
-% Loop through the variable names and copy data from input_data if it exists
-for i = 1:numel(templateVarNames)
-    varName = templateVarNames{i};
-    if ismember(varName, input_data.Properties.VariableNames)
-        outputDataCell{i} = input_data{1, varName};
-    elseif strcmp(varName, 'tania_subid')
-        % Generate tania_subid based on sub_fn, sub_ln, and dob
-        fn = input_data.sub_fn{1};
-        ln = input_data.sub_ln{1};
-        dob = input_data.dob(1);
+% Get today's date
+todayDate = datetime('now', 'Format', 'MM/dd/yyyy');
 
-        % Your code to generate tania_subid
-        %get the first letter of their first and last names
-        fn_letter = fn(1);
-        ln_letter = ln(1);
-        %convert letters to corresponding number value
-        alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        fn_num = strfind(alphabet, fn_letter);
-        ln_num = strfind(alphabet, ln_letter);
-        %pad the numbers with 0
-        fn_padded_num = sprintf('%02d', fn_num);
-        ln_padded_num = sprintf('%02d', ln_num);
-        dob = input_data.dob(1); % Ensure it's a string
-        dob = char(dob); % Convert to a character array
-        %get the last two digits of their dob
-        dob_num = dob(end-1:end);
-        % Generate tania_subid
-        tania_subid = [fn_padded_num, ln_padded_num, dob_num];
-
-        outputDataCell{i} = tania_subid;
-    else
-        outputDataCell{i} = ""; % Or any other default value
+% Loop through each row in the input_data
+for row = 1:height(input_data)
+    % Loop through the variable names and copy data from input_data if it exists
+    for i = 1:numel(templateVarNames)
+        varName = templateVarNames{i};
+        if ismember(varName, input_data.Properties.VariableNames)
+            value = input_data{row, varName};
+            % Replace NaN values with empty string
+            if isnumeric(value) && isnan(value)
+                value = '';
+            end
+            outputDataCell{row, i} = value;
+        elseif strcmp(varName, 'tania_subid')
+            % Generate tania_subid based on sub_fn, sub_ln, and dob for each row
+            fn = input_data.sub_fn{row};
+            ln = input_data.sub_ln{row};
+            dob = input_data.dob(row); % Access the date value
+            dob = datestr(dob, 'mmddyy'); % Convert date to string
+            
+            %get the first letter of their first and last names
+            fn_letter = fn(1);
+            ln_letter = ln(1);
+            %convert letters to corresponding number value
+            alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            fn_num = strfind(alphabet, fn_letter);
+            ln_num = strfind(alphabet, ln_letter);
+            %pad the numbers with 0
+            fn_padded_num = sprintf('%02d', fn_num);
+            ln_padded_num = sprintf('%02d', ln_num);
+            dob = char(dob); % Convert to a character array
+            %get the last two digits of their dob
+            dob_num = dob(end-1:end);
+            % Generate tania_subid
+            tania_subid = [fn_padded_num, ln_padded_num, dob_num];
+            
+            outputDataCell{row, i} = tania_subid;
+        % Recontact Status = Needs to be recontacted
+        elseif strcmp(varName, 'recontact_status')
+            outputDataCell{row, i} = 1;
+        % Com log = Complete
+        elseif strcmp(varName, 'communication_log_complete')
+            outputDataCell{row, i} = 2; 
+        % Fill date_recontact with today's date
+        elseif strcmp(varName, 'date_recontact')
+            outputDataCell{row, i} = todayDate;
+        else
+            outputDataCell{row, i} = "";
+        end
     end
 end
 
@@ -50,4 +68,5 @@ end
 % and data from input_data (where they match)
 output_data = cell2table(outputDataCell, 'VariableNames', templateVarNames);
 
+% Write the output_data to 'output.csv'
 writetable(output_data, 'output.csv')
